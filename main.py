0999d5655b8fcc1f82922ff374c11f8a80d473af
«Erik <erik.vasilyan.7e6@itb.cat>
2023-10-30 19:56:28 +0100

    Comments added to identify the authors and project

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy
+import matplotlib
+import pandas
+
+# TITLE: MP15-UF1-AC04 - Exercicis Python IV
+# NAME: Erik Vasilyan, Marçal Herraiz
+# DATA: 2023-10-30
+
+
+def fetch_csv_data(csv_file_path):
+    return 0
